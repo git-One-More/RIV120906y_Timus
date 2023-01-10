c0344ff878a091a0e95283536916e870ea310b97
Denis Makhonin <mahonindenis1@gmail.com>
2023-01-10 15:17:48 +0500

    initial commit

--- src/Timus/Ex1003_Parity.java
+++ src/Timus/Ex1003_Parity.java
@@ -1,27 +1,19 @@
 package Timus;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-
 public class Ex1003_Parity {
-
-    
     public static class DSU {
-        
         Map<Integer, Integer> root = new HashMap<Integer, Integer>();
         Map<Integer, Integer> size = new HashMap<Integer, Integer>();
-        
         private int getRoot(int i) {
             while (root.get(i) != i) {
                 i = root.get(i);
             }
             return i;
         }
-        
         public int union(int i, int j) {
-            
             if (root.get(i) == null) {
                 root.put(i, i);
                 size.put(i, 1);
@@ -30,15 +22,12 @@ public class Ex1003_Parity {
                 root.put(j, j);
                 size.put(j, 1);
             }
-            
             int root_i = getRoot(i);
             int root_j = getRoot(j);
             int parent = 0;
-            /* same set then just return the parent */
             if (root_i == root_j) {
                 parent = root_i;
             }
-            /* Else different sets */
             else if (size.get(root_i) > size.get(root_j)) {
                 parent = root_i;
                 root.put(root_j, parent);
@@ -46,10 +35,8 @@ public class Ex1003_Parity {
                 parent = root_j;
                 root.put(root_i, parent);
             }
-            
             return parent;
         }
-        
         public boolean find(int i, int j) {
             if (root.get(i) != null && root.get(j) != null) {
                 return getRoot(i) == getRoot(j);
